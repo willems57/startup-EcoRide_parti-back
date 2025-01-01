4449ab1a077c35e342274023ad5690e86f7d9485
willems57 <jeanwillems@hotmail.fr>
2025-01-01 15:09:23 +0100

    finition imparfaite

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,49 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Avis;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
-class UserFixtures extends Fixture
-{
-    public function load(ObjectManager $manager): void
+class UserFixtures extends Fixture implements FixtureGroupInterface
+
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        public const USER_NB_TUPLES = 20;
+    
+        public function __construct(private UserPasswordHasherInterface $passwordHasher)
+        {
+        }
+    
+        public function load(ObjectManager $manager): void
+        {
+            $faker = Factory::create();
+    
+            $avis = new Avis();
+        $manager->persist($avis);
+            for ($i = 1; $i <= self::USER_NB_TUPLES; $i++) {
+                $user = (new User())
+                    ->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName())
+                    ->setRoles($faker->Roles()) // Définition des rôles
+                    ->setEmail($faker->unique()->safeEmail())
+                    ->setAvis($avis)
+                    ->setCreatedAt(new \DateTimeImmutable()); // Définir createdAt
+    
+                $user->setPassword($this->passwordHasher->hashPassword($user, 'password' . $i));
+    
+                $manager->persist($user);
+            }
+    
+            $manager->flush();
+        }
+    
+        public static function getGroups(): array
+        {
+            return ['independent'];
+        }
     }
-}
